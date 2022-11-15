a6244e68a69a251f98754ec532624e9fb2d367ae
RiyadQaraxanov <riyadqaraxanov9@gmail.com>
2022-11-15 21:56:38 +0400

    Search Component 2

--- screens/search/components/Body.js
+++ screens/search/components/Body.js
@@ -30,6 +30,26 @@ const Body = () => {
                     <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/3,borderWidth:0.5}}></View>
                     <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/3,borderWidth:0.5}}></View>
                 </View>
+                <View>
+                    <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/3,borderWidth:0.5}}></View>
+                    <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/3,borderWidth:0.5}}></View>
+                </View>
+                <View>
+                    <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/3,borderWidth:0.5}}></View>
+                    <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/3,borderWidth:0.5}}></View>
+                </View>
+                <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/1.5,borderWidth:0.5}}></View>
+            </View>
+            <View style={{flexDirection:"row"}}>
+                <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/1.5,borderWidth:0.5}}></View>
+                <View>
+                    <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/3,borderWidth:0.5}}></View>
+                    <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/3,borderWidth:0.5}}></View>
+                </View>
+                <View>
+                    <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/3,borderWidth:0.5}}></View>
+                    <View style={{backgroundColor:"red",width:WIDTH/3,height:WIDTH/3,borderWidth:0.5}}></View>
+                </View>
             </View>
         </View>
     )
